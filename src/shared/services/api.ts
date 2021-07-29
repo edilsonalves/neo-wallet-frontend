31271da97ff8ed20ffd663aae1a4148092f46418
Edilson Alves <email@edilsonalves.com>
2021-07-29 14:50:56 -0300

    refactor: add axios interceptors

--- src/shared/services/api.ts
+++ src/shared/services/api.ts
@@ -1,9 +1,38 @@
 import axios from 'axios';
+import { createBrowserHistory } from 'history';
 
 const apiBaseUrl = process.env.REACT_APP_API_BASE_URL;
 
 const api = axios.create({
   baseURL: apiBaseUrl,
 });
+
+api.interceptors.request.use(
+  async (config) => {
+    const userToken = localStorage.getItem('@NeoWallet:token');
+    config.headers.Authorization = `Bearer ${userToken}`;
+
+    return config;
+  },
+  async (error) => {
+    return Promise.reject(error);
+  }
+);
+
+const history = createBrowserHistory();
+
+api.interceptors.response.use(
+  async (response) => {
+    return response;
+  },
+  async (error) => {
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem('@NeoWallet:token');
+      history.push('/');
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 export default api;
